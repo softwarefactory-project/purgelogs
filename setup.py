2bd3fb3c7646d97a0104045de8fc8a8437de085a
Fabien Boucher <fboucher@redhat.com>
2023-05-03 10:15:02 +0000

    Bump version to 0.2.3
    Due to a mistaske in release process the change bump
    the seteup.py from 0.2.1 to 0.2.3.
    
    Change-Id: I8eda7f4a1579f5a0c61dfb3065a8ea356d47b124

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='purgelogs',
-    version='0.2.1',
+    version='0.2.3',
     py_modules=['purgelogs'],
     entry_points = {
         'console_scripts': ['purgelogs=purgelogs:main']
